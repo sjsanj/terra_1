2aec0f846dca5b6041f1963d272e40c4643074ae
root <root@ip-172-31-16-187.ec2.internal>
2022-07-23 10:41:22 +0000

    new

--- main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-    region = "us-east-1"
-}
-
-resource "aws_instance" "terra_2" {
-    ami = var.ami_id   
-    instance_type = var.instance_type  
-  
-}
--- output.tf
+++
@@ -1,4 +0,0 @@
-output "public_ip" {
-    value = aws_instance.terra_2.public_ip
-    description = "Public IP Address of Instance"
-}
