ab370d7fcac6bc159e57b901ebcd27042b7ead79
sjsanj <107824358+sjsanj@users.noreply.github.com>
2022-07-23 13:38:30 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,18 @@
+variable "ami_id" {
+    default = "ami-08d4ac5b634553e16"  
+
+variable "instance_type" {
+    default = "t2.micro"
+}
+
+variable "vpc_id" {
+    default = "vpc-0f2df21182680da97"    
+}
+
+variable "port" {
+    default = 22
+}
+
+variable "cidr_block" {
+    default = "0.0.0.0/0"
+}
