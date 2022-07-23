a2c321082cf3e484cf7e6bad64db998098aaa462
sjsanj <107824358+sjsanj@users.noreply.github.com>
2022-07-23 16:19:23 +0530

    Update main.tf

--- ec_2_module/main.tf
+++ ec_2_module/main.tf
@@ -5,23 +5,5 @@ provider "aws" {
 resource "aws_instance" "terra_2" {
     ami = var.ami_id   
     instance_type = var.instance_type  
-    vpc_security_group_ids = [aws_security_group.mysg.id]
-}
-
-resource "aws_security_group" "mysg" {
-    name = "allow-ssh"
-    description = "Allow ssh traffic"
-    vpc_id = var.vpc_id  
-    ingress {
-        description = "Allow inbound ssh traffic"
-        from_port = var.port
-        to_port = var.port
-        protocol = "tcp"
-
-        cidr_blocks = [var.cidr_block]
-    }
-
-    tags = {
-        name = "allow_ssh"
-    }
+  
 }
